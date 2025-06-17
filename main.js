910c08d65303c765bfc375565830f009cb89f501
AL Tarek Sofian dev <sofiandev815@gmail.com>
2025-06-17 05:20:22 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// Loops:-
+/* 
+  for loop
+  Nested foor loop
+  while loop
+ */
+
+// For loop
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log("================================");
+
+for (let i = 10; i >= 0; i--) {
+  console.log(i);
+}
+
+console.log(`===================================
+  =========================================`);
+
+let i = 0;
+
+while (i <= 100) {
+  console.log(i);
+  i++;
+}
